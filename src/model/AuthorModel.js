2a9d4afab711a00357ddd509e670be97e89f2b0a
soorajptk <soorajptk1997@gmail.com>
2022-01-02 00:43:52 -0800

    final

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Nayana:Nayana95@ictaktrial.nyh4v.mongodb.net/ERROR_HUNT?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://soorajptk:Soorajptk%40upaobca011@nodeexpressprojects.v3g28.mongodb.net/LibraryApp-1997?retryWrites=true&w=majority',{
     useUnifiedTopology:true,
     useNewUrlParser:true,
-    useFindAndModify:false//part#2 point9
+    useFindAndModify:false,//part#2 point9
 });
 const Schema = mongoose.Schema;
 
--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Nayana:Nayana95@ictaktrial.nyh4v.mongodb.net/ERROR_HUNT?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://soorajptk:Soorajptk%40upaobca011@nodeexpressprojects.v3g28.mongodb.net/LibraryApp-1997?retryWrites=true&w=majority',{
     useUnifiedTopology:true,
     useNewUrlParser:true,
     useFindAndModify:false//part#2 point9
